1535eb78ec3de989d7dcf693ccf0152dfd95b71f
simplenick <simplenick01@gmail.com>
2022-07-02 07:10:01 +0100

    Added Some Tests

--- InfoWeb/staff/tests.py
+++ InfoWeb/staff/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class StaffViewTests(TestCase):
+    def test_home_view(self):
+        """Check that home view is working"""
+        home = self.client.get(reverse('staff:home'))
+        self.assertEqual(home.status_code, 200)
+
+    def test_login_view(self):
+        """Check that login view works"""
+        login = self.client.get(reverse('staff:login'))
+        self.assertEqual(login.status_code, 200)
+
+    def test_register_view(self):
+        """Check that Register View Works"""
+        register = self.client.get(reverse('staff:register'))
+        self.assertEqual(register.status_code, 200)
+
