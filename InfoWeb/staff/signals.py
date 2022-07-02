ea114e766e4bfd2f4e336d9a85db7416116ff822
simplenick <simplenick01@gmail.com>
2022-07-02 07:48:39 +0100

    modified signal: add new registered user to staff group

--- InfoWeb/staff/signals.py
+++ InfoWeb/staff/signals.py
@@ -2,16 +2,16 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 from django.contrib.auth.models import Group
-from .models import PersonalDetails
+from .models import InfoWebUser
 
 
-def customer_profile(sender, instance, created, **kwargs):
+def staff_profile(sender, instance, created, **kwargs):
   if created:
     group = Group.objects.get(name='staff')
     instance.groups.add(group)
     
-    PersonalDetails.objects.create(user=instance, name=instance.username)
-    print('\nCustomer Created\n')
+    InfoWebUser.objects.create(name=instance.username)
+    print('\nStaff Created\n')
     
-post_save.connect(customer_profile, sender=User)
+post_save.connect(staff_profile, sender=User)
   
