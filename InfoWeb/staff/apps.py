6b9bbdf8df8febe001859715f5a5a358591868e2
simplenick <simplenick01@gmail.com>
2022-07-02 07:07:35 +0100

    Added some signals for post save on new_users

--- InfoWeb/staff/apps.py
+++ InfoWeb/staff/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class StaffConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'staff'
+
+    def ready(self):
+        import staff.signals
--- InfoWeb/staff/signals.py
+++ InfoWeb/staff/signals.py
@@ -1,21 +1,17 @@
-from unicodedata import name
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+
 from django.contrib.auth.models import Group
+from .models import PersonalDetails
 
 
-from .models import PersonalDetails, WorkDetails
-
-def staff_profile(sender, instance, created, **kwargs):
-    if created:
-        group = Group.objects.get(name='staff')
-        instance.groups.add(group)
-        PersonalDetails.objects.create(
-            user=instance,
-			name=instance.username,
-        )
-
-        print(f"\n New Staff Created and added to {group}")
-
-
-post_save.connect(staff_profile, sender=User)
+def customer_profile(sender, instance, created, **kwargs):
+  if created:
+    group = Group.objects.get(name='staff')
+    instance.groups.add(group)
+    
+    PersonalDetails.objects.create(user=instance, name=instance.username)
+    print('\nCustomer Created\n')
+    
+post_save.connect(customer_profile, sender=User)
+  
