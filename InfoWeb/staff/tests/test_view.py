eaf3103b24956a654107717552ab2e1dd56e348d
simplenick <simplenick01@gmail.com>
2022-07-06 02:08:46 +0100

    Added a test Suite for Stadds

---
+++ InfoWeb/staff/tests/test_view.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class StaffViewTests(TestCase):
+    def test_home_view(self):
+        """Check that home view is working"""
+        home = self.client.get(reverse('staff:home'))
+        self.assertEqual(home.status_code, 200)
+
+    def test_login_view(self):
+        """Check that login view works"""
+        login = self.client.get(reverse('staff:login'))
+        self.assertEqual(login.status_code, 200)
+
+    def test_register_view(self):
+        """Check that Register View Works"""
+        register = self.client.get(reverse('staff:register'))
+        self.assertEqual(register.status_code, 200)
+
+    def test_dashboard_view_anonymous(self):
+        """Check that Dashboard doesn't Work for anonymous users"""
+        dashboard = self.client.get(reverse('staff:dashboard'))
+        self.assertEqual(dashboard, 404)
