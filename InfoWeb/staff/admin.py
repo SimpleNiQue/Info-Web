51d0b1a35f0d84f977a428df18ea9e329adcd321
simplenick <simplenick01@gmail.com>
2022-07-12 00:17:26 +0100

    Removed old app

--- InfoWeb/staff/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from.models import WorkDetails, PersonalDetails, InfoWebUser
-
-admin.site.register(PersonalDetails)
-admin.site.register(WorkDetails)
-admin.site.register(InfoWebUser)
